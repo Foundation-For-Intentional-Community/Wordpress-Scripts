39cad9bcc9d03a91a77d525647bbe7caa595c9d9
Pavan Rikhi <pavan.rikhi@gmail.com>
2019-08-27 20:00:16 -0400

    Add Created Date to FormItem Model

--- src/Schema.hs
+++ src/Schema.hs
@@ -90,6 +90,7 @@ FormItem sql=3uOgy46w_frm_items
     post PostId Maybe sql=post_id
     user UserId Maybe sql=user_id
     isDraft Bool sql=is_draft
+    createdAt UTCTime sql=created_at
     updatedAt UTCTime sql=updated_at
     deriving Show
 
